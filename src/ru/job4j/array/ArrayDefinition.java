5ccca9d9069572e94baee1fca6dff01209ba9d82
Darya Leonteva <darya.leonteva@rt.ru>
2021-02-21 23:17:34 +0300

    6.0.2. Заполнение массива. [#462626]

--- src/ru/job4j/array/ArrayDefinition.java
+++ src/ru/job4j/array/ArrayDefinition.java
@@ -9,5 +9,15 @@ public class ArrayDefinition {
         System.out.println("Размер массива ages равен: " + ages.length);
         System.out.println("Размер массива surnames равен: " + surnames.length);
         System.out.println("Размер массива prices равен: " + prices.length);
+
+        String[] names = new String[4];
+        names[0] = "Darya";
+        names[1] = "Vladimir";
+        names[2] = "Valentina";
+        names[3] = "Petr";
+        System.out.println(names[0]);
+        System.out.println(names[1]);
+        System.out.println(names[2]);
+        System.out.println(names[3]);
     }
 }
